927f1bc2d288a87341b81e0baa4f80ad0c37e2f3
立青木白 <cywhoyi@gmail.com>
2019-12-19 11:33:29 +0800

    :apple::whale::penguin::checked_flag: redirect to homepage

--- api/src/main.rs
+++ api/src/main.rs
@@ -63,13 +63,13 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn user_handler(event: UserEvent, context: lambda::Context) -> Result<CustomOutput, HandlerError> {
-    if e.event_type == 0 {
+    if event.event_type == 0 {
         return home_handler(event, context);
     }
-    else if e.event_type == 1 {
+    else if event.event_type == 1 {
         return get_user_handler(event, context);
     }
-    else if e.event_type == 2 {
+    else if event.event_type == 2 {
         return user_handler_for_send_user_code(event, context);
     }
     else {
@@ -78,11 +78,9 @@ fn user_handler(event: UserEvent, context: lambda::Context) -> Result<CustomOutp
 }
 
 
-fn home_handler(e: UserEvent, c: lambda::Context) -> Result<CustomOutput, HandlerError> {
-    let mut map = HashMap::new();
+fn home_handler(event: UserEvent, context: lambda::Context) -> Result<CustomOutput, HandlerError> {
     let mut response=reqwest::Client::new()
     .post(API_BASE_USER_URL)
-    .json(&map)
     .send()
     .unwrap();
     // copy the response body directly to stdout
@@ -100,25 +98,25 @@ fn home_handler(e: UserEvent, c: lambda::Context) -> Result<CustomOutput, Handle
 
 
 
-fn get_user_handler(e: UserEvent, c: lambda::Context) -> Result<CustomOutput, HandlerError> {
+fn get_user_handler(event: UserEvent, context: lambda::Context) -> Result<CustomOutput, HandlerError> {
 
-    if e.mobile == "" && e.user_name == "" {
-        error!("Empty user mobile and user name  in request {}", c.aws_request_id);
-        return Err(c.new_error("both mobile and username are empty"));
+    if event.mobile == "" && event.user_name == "" {
+        error!("Empty user mobile and user name  in request {}", context.aws_request_id);
+        return Err(context.new_error("both mobile and username are empty"));
     }
     // if e.user_name == "" {
     //     error!("Empty user name in request {}", c.aws_request_id);
     //     return Err(c.new_error("Empty username"));
     // }
-    if e.password == "" {
-        error!("Empty user password in request {}", c.aws_request_id);
-        return Err(c.new_error("Empty password"));
+    if event.password == "" {
+        error!("Empty user password in request {}", context.aws_request_id);
+        return Err(context.new_error("Empty password"));
     }
     let mut map = HashMap::new();
-    map.insert("userName", e.user_name);
-    map.insert("code", e.code.to_string());
-    map.insert("password", e.password);
-    map.insert("mobile", e.mobile);
+    map.insert("userName", event.user_name);
+    map.insert("code", event.code.to_string());
+    map.insert("password", event.password);
+    map.insert("mobile", event.mobile);
     let mut response=reqwest::Client::new()
     .post(API_BASE_USER_URL)
     .json(&map)
@@ -136,15 +134,15 @@ fn get_user_handler(e: UserEvent, c: lambda::Context) -> Result<CustomOutput, Ha
     })
 }
 
-fn user_handler_for_send_user_code(e: UserEvent, c: lambda::Context) -> Result<CustomOutput, HandlerError> {
+fn user_handler_for_send_user_code(event: UserEvent, context: lambda::Context) -> Result<CustomOutput, HandlerError> {
 
-    if e.mobile == "" {
-        error!("Empty user mobile in request {}", c.aws_request_id);
-        return Err(c.new_error("Empty mobile"));
+    if event.mobile == "" {
+        error!("Empty user mobile in request {}", context.aws_request_id);
+        return Err(context.new_error("Empty mobile"));
     }
 
     let mut map = HashMap::new();
-    map.insert("code", e.code.to_string());
+    map.insert("code", event.code.to_string());
     let mut response=reqwest::Client::new()
     .post(API_BASE_USERCODE_URL)
     .json(&map)
@@ -161,26 +159,26 @@ fn user_handler_for_send_user_code(e: UserEvent, c: lambda::Context) -> Result<C
     })
 }
 
-fn user_handler_registe_user(e: UserEvent, c: lambda::Context) -> Result<CustomOutput, HandlerError> {
+fn user_handler_registe_user(event: UserEvent, context: lambda::Context) -> Result<CustomOutput, HandlerError> {
 
-    if e.mobile == "" {
-        error!("Empty user mobile in request {}", c.aws_request_id);
-        return Err(c.new_error("Empty mobile"));
+    if event.mobile == "" {
+        error!("Empty user mobile in request {}", context.aws_request_id);
+        return Err(context.new_error("Empty mobile"));
     }
 
-    if e.user_name == "" {
-        error!("Empty user name in request {}", c.aws_request_id);
-        return Err(c.new_error("Empty username"));
+    if event.user_name == "" {
+        error!("Empty user name in request {}", context.aws_request_id);
+        return Err(context.new_error("Empty username"));
     }
-    if e.password == "" {
-        error!("Empty user password in request {}", c.aws_request_id);
-        return Err(c.new_error("Empty password"));
+    if event.password == "" {
+        error!("Empty user password in request {}", context.aws_request_id);
+        return Err(context.new_error("Empty password"));
     }
     let mut map = HashMap::new();
-    map.insert("userName", e.user_name);
-    map.insert("code", e.code.to_string());
-    map.insert("password", e.password);
-    map.insert("mobile", e.mobile);
+    map.insert("userName", event.user_name);
+    map.insert("code", event.code.to_string());
+    map.insert("password", event.password);
+    map.insert("mobile", event.mobile);
     let mut response=reqwest::Client::new()
     .post(API_BASE_REGISTER_URL)
     .json(&map)
